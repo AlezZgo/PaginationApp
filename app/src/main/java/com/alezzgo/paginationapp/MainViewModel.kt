2b53b8f006a1b6b753c342891afe2288bdec58c3
Александр <alez.grante@gmail.com>
2023-03-05 13:06:15 +0300

    Google code lab

--- app/src/main/java/com/alezzgo/paginationapp/MainViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.alezzgo.paginationapp
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-
-    fun get() = "text"
-}
---
+++ app/src/main/java/com/alezzgo/paginationapp/data/GithubRepository.kt
@@ -0,0 +1,45 @@
+package com.alezzgo.paginationapp.data
+
+import android.util.Log
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.alezzgo.paginationapp.data.cloud.GithubService
+import com.alezzgo.paginationapp.data.db.RepoDatabase
+import com.alezzgo.paginationapp.data.model.Repo
+import kotlinx.coroutines.flow.Flow
+
+
+class GithubRepository(
+    private val service: GithubService,
+    private val database: RepoDatabase
+) {
+
+    /**
+     * Search repositories whose names match the query, exposed as a stream of data that will emit
+     * every time we get more data from the network.
+     */
+    fun getSearchResultStream(query: String): Flow<PagingData<Repo>> {
+        Log.d("GithubRepository", "New query: $query")
+
+        // appending '%' so we can allow other characters to be before and after the query string
+        val dbQuery = "%${query.replace(' ', '%')}%"
+        val pagingSourceFactory = { database.reposDao().reposByName(dbQuery) }
+
+        @OptIn(ExperimentalPagingApi::class)
+        return Pager(
+            config = PagingConfig(pageSize = NETWORK_PAGE_SIZE, enablePlaceholders = false),
+            remoteMediator = GithubRemoteMediator(
+                query,
+                service,
+                database
+            ),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow
+    }
+
+    companion object {
+        const val NETWORK_PAGE_SIZE = 30
+    }
+}
